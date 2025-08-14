580afd170b7af3a20a232377a173245cb926864a
ajmerasaini01 <ajmerasaini01@gmail.com>
2025-08-14 15:38:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,10 +11,4 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "5298142728"))
 CREDIT = environ.get("CREDIT", "saini bots")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5298142728').split(',')
-TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5298142728').split(',')
-AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-if int(OWNER) not in AUTH_USERS:
-    AUTH_USERS.append(int(OWNER))
