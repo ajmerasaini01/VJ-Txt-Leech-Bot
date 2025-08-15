e5f3e7de49db5c18a7c17734c41bfd2dbab5c6f2
ajmerasaini01 <ajmerasaini01@gmail.com>
2025-08-15 23:02:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,7 +8,8 @@ API_ID = int(environ.get("API_ID", "26331872"))
 API_HASH = environ.get("API_HASH", "c93589620441707c37c5683a02eea54e")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "5298142728"))
-CREDIT = environ.get("CREDIT", "saini bots")
+OWNER = int(environ.get("OWNER", "8355707251"))
+CREDIT = environ.get("CREDIT", "saini★ bots")
+
 
 
